0aec09165a0890294a52c2cf58ad02cb3b651df7
Harshitha Shetty <141444342+HarshithaShetty27@users.noreply.github.com>
2024-09-15 02:40:00 +0530

    Added gitignore and other things

---
+++ server/config/connectDB.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose')
+
+async function connectDB(){
+    try{
+        await mongoose.connect(process.env.MONGOOSE_URI)
+
+        const connection= mongoose.connection
+        connection.on('connected',()=>{
+            console.log("Connected to Database")
+        })
+        connection.on('error',(error)=>{
+            console.log("Something went wrong in MongoDB",error)
+        })
+    } catch(error){
+        console.log("Something is wrong",error)
+    }
+}
+
+module.exports = connectDB

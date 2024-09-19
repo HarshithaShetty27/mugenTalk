51d9cd3c3ac1ce1c76412158958d79da2f7ba46a
Harshitha Shetty <141444342+HarshithaShetty27@users.noreply.github.com>
2024-09-20 02:48:53 +0530

    Avatar,CheckPassword-half

--- client/src/pages/CheckEmailPage.js
+++ client/src/pages/CheckEmailPage.js
@@ -44,7 +44,9 @@ const CheckEmailPage = () => {
           email: ""
         })
 
-        navigate('/password')
+        navigate('/password',{
+          state : response?.data?.data
+        })
       }
 
     } catch (error) {

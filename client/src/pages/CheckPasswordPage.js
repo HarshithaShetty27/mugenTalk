220d70faef12be2920ee7e9d48cb6bb7106a2a49
Harshitha Shetty <141444342+HarshithaShetty27@users.noreply.github.com>
2024-10-09 02:07:52 +0530

    Redux- store, userSlice index.js, checkPasswordPage ( 1:44 a.m. : o  )

--- client/src/pages/CheckPasswordPage.js
+++ client/src/pages/CheckPasswordPage.js
@@ -6,6 +6,8 @@ import axios from 'axios'
 import toast from 'react-hot-toast';
 import { PiUserCircle } from "react-icons/pi";
 import Avatar from '../components/Avatar';
+import { useDispatch } from 'react-redux';
+import { setToken, setUser } from '../redux/userSlice';
 
 
 const CheckPasswordPage = () => {
@@ -17,6 +19,7 @@ const CheckPasswordPage = () => {
 
   const navigate = useNavigate()
   const location = useLocation()
+  const dispatch = useDispatch()
 
   console.log("location", location.state)
 
@@ -58,6 +61,9 @@ const CheckPasswordPage = () => {
       toast.success(response.data.message)
 
       if (response.data.success) {
+        dispatch(setToken(response?.data?.token))
+        localStorage.setItem('token', response?.data?.token)
+
         setData({
           password: ""
         })

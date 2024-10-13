f647dbb500eff65372f0e35db6014f5c69b42fe9
Harshitha Shetty <141444342+HarshithaShetty27@users.noreply.github.com>
2024-10-13 23:05:04 +0530

    Home page design- Sidebar, EditUser

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -3,6 +3,7 @@ import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { Outlet, useNavigate } from 'react-router-dom'
 import { logout, setUser } from '../redux/userSlice'
+import Sidebar from '../components/Sidebar'
 
 const Home = () => {
   const user = useSelector(state => state.user)
@@ -36,8 +37,10 @@ const Home = () => {
     fetchUserDetails()
   },[])
   return (
-    <div>
-      Home
+    <div className='grid lg:grid-cols-[300px,1fr] h-screen max-h-screen'>
+      <section className='bg-white'>
+        <Sidebar/>
+      </section>
 
       {/* Message Component */}
       <section>
